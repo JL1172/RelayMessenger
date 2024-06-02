7487146606f8aa5690e40ea63f2973873db952da
Jacob Lang <130790079+JL1172@users.noreply.github.com>
2024-06-02 06:18:10 -0400

    fixed server is full issue

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,19 +4,20 @@ const port: number = Number(process.env.PORT) || 8080;
 
 const wss = new WebSocketServer({ port });
 let activeClients: WebSocket[] = [];
-
+console.clear();
 wss.on("connection", (ws: WebSocket) => {
-  if (activeClients.length > 2) {
-    ws.send("Server Full.");
-    ws.close();
-    return;
-  }
+  // if (activeClients.length > 2) {
+  //   ws.send("Server Full.");
+  //   ws.close();
+  //   return;
+  // }
   activeClients.push(ws);
   console.log('Connected');
 
   ws.on("message", (message: string) => {
     activeClients.forEach((client: WebSocket) => {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
+        console.log(message);
         client.send(message);
       }
     });
